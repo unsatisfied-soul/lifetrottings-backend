7fe84137fac5e3a922989326e05a311bc26d7b62
Unsatisfied Soul <shovontunvir43@gmail.com>
2022-01-27 20:39:42 +0600

    add data

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const { MongoClient } = require('mongodb');
+const bodyParser = require("body-parser");
 const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
+app.use(bodyParser.json());
 
 
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h6wi6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -14,25 +16,31 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect()
-        const  travelExperince= client.db('travelExperinece').collection('travelExperince')
+        const lifetrooting =  client.db('lifetrooting')
+        const travelExperince = lifetrooting.collection('travelExperince')
 
-        app.post('/travleExperience', async(req,res)=> {
+        app.post('/experience', async(req,res)=> {
+            console.log(req.body)
             const title = req.body.title
             const desc = req.body.desc
-            const writer = req.body.writer
             const date = req.body.date
-            const image = req.body.image
+            const writer = req.body.writer
+            const travelImage = req.body.travelImage
             // const bufferImage = Buffer.from(encodepic,'base64')
-            console.log(title,desc, writer,date,image)
             const travelExperincedata = {
                 title,
-                writer,
                 desc,
                 date,
-                image
+                writer,
+                travelImage
             }
             const result = await travelExperince.insertOne(travelExperincedata)
             res.json(result)
+        })
+        app.get('/travleExperience', async(req,res)=> {
+            const cursor = travelExperince.find({})
+            const travelingplace = await cursor.toArray()
+            res.send(travelingplace)
         })
         
 
