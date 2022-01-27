9e67c745a8a7bf3cb2110f6e9945be68cd13ca51
Unsatisfied Soul <shovontunvir43@gmail.com>
2022-01-27 13:15:40 +0600

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { MongoClient } = require('mongodb');
+const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.use(cors())
 app.use(express.json())
 
 
@@ -17,6 +18,24 @@ async function run(){
         const lifetrottingDb = client.db("lifetrotting").collection("travelPlaces");
         const  travelExperince= client.db('travelExperinece').collection('travelOrder')
 
+        app.post('/travleExperience', async(req,res)=> {
+            const title = req.body.title
+            const desc = req.body.desc
+            const writer = req.body.writer
+            const date = req.body.date
+            const image = req.body.image
+            const encodepic = req.body.image
+            // const bufferImage = Buffer.from(encodepic,'base64')
+            const travelExperincedata = {
+                title,
+                writer,
+                desc,
+                date,
+                image
+            }
+            const result = await travelExperince.insertOne(travelExperincedata)
+            res.json(result)
+        })
         
 
     }
